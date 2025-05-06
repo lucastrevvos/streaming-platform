3a19214687dd9bbc4915e382ccf082d34ea499b9
Lucas Amaral <lucas.trevvos@gmail.com>
2025-05-06 08:17:38 -0300

    fix: proteção do endpoint externo com login

--- backend/src/routes/externalRoutes.ts
+++ backend/src/routes/externalRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { getAlbumsByArtist } from "../controllers/externalController";
+import { verifyToken } from "../middlewares/verifyToken";
 
 const router = Router();
+
+router.use(verifyToken);
 
 router.get("/albums/:artist", getAlbumsByArtist);
 
